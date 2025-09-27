2e30a39df47766a685b50434785ce5d0ac56f16c
Marcos Vinicius <marcosmarcosviniciusdigital@gmail.com>
2025-09-27 13:19:31 -0300

    Prisma-intergrantion

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
-import { urlencoded } from "body-parser";
+import bodyParser, { urlencoded } from "body-parser";
 import express from "express";
 import { router } from "./routes/index";
+import cors from "cors";
 
 const app = express();
-
+app.use(cors());
 app.use(urlencoded({ extended: true }));
 app.disable("x-power-by");
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(express.static("public"));
 app.use(router);
 const port = process.env.PORT;
 
